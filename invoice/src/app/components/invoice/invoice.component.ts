42515366a820141ee6a5ab3de03a58dc7e43f481
ArevaloDev <arevalodaniel999@gmail.com>
2025-04-24 19:23:35 -0600

    maquetando facturacion

--- invoice/src/app/components/invoice/invoice.component.ts
+++ invoice/src/app/components/invoice/invoice.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Invoice } from '../../interfaces/invoice.interface';
+import { InvoiceService } from '../../services/invoice.service';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit{
+  public invoice!:Invoice
 
+  constructor(private invoiceServices:InvoiceService){}
+
+  ngOnInit(): void {
+    this,this.invoice = this.invoiceServices.getInvoice();
+  }
 }
--- invoice/src/app/interfaces/item.interface.ts
+++ invoice/src/app/interfaces/item.interface.ts
@@ -2,5 +2,5 @@ export interface Item{
   id:number;
   product:string;
   price:number;
-  quatity:number;
+  quantity:number;
 }
