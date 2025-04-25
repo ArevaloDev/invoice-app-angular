d0b8de7cb308d1c45d7bda1783c8f43e96d9ceed
ArevaloDev <arevalodaniel999@gmail.com>
2025-04-24 18:09:12 -0600

    agregando interfaces para facturacion

---
+++ invoice/src/app/interfaces/client.interface.ts
@@ -0,0 +1,7 @@
+import { Address } from "./address.interface";
+
+export  interface Client{
+  name:string;
+  lastName:string;
+  address:Address;
+}
---
+++ invoice/src/app/interfaces/invoice.interface.ts
@@ -0,0 +1,11 @@
+import { Client } from "./client.interface";
+import { Company } from "./company.interface";
+import { Item } from "./item.interface";
+
+export interface Invoice{
+  id:number;
+  name:string;
+  client:Client;
+  company:Company;
+  items:Item[];
+}
